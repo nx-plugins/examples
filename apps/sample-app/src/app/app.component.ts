77d72c3a38c6fa93102f2641836c6872fce3d771
Sebastian Guevara <info@sebasg.dev>
2020-03-17 06:24:28 -0500

    initial commit

---
+++ apps/sample-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-plugins-examples-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'sample-app';
+}
